d09dff39e90eea8b137cdd97e7daba5d2b3c33eb
Sebastian Gebski <sebastian@gebski.net>
2023-05-17 21:17:27 +0200

    Successful call of Translate via AWS SDK for Kotlin

--- src/main/kotlin/sandbox/api/TranslateController.kt
+++ src/main/kotlin/sandbox/api/TranslateController.kt
@@ -3,14 +3,19 @@ package sandbox.api
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import aws.sdk.kotlin.services.translate.TranslateClient
+import aws.sdk.kotlin.services.translate.model.TranslateTextRequest
 
 @Controller("/translate")
 class TranslateController {
 
     @Get(produces = ["text/plain"])
     suspend fun index(): String {
-        // val translateClient = TranslateClient { region = "eu-central-1" }
-        // return translateClient.translateText("Hello World", "de", "en").translatedText ?: "No translation"
-        return "Hello World"
+        TranslateClient { region = "eu-central-1" }.use { client ->
+            return client.translateText(TranslateTextRequest {
+                sourceLanguageCode = "en"
+                targetLanguageCode = "de"
+                text = "Hello World"
+            }).translatedText.orEmpty()
+        }
     }
 }
